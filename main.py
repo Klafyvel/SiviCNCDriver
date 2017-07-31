b89772c5123b50f00cffa2e08a4ace4a23109315
Klafyvel <klafyvel@gmail.com>
2017-07-31 14:11:40 +0200

    gitignore

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 import settings
-from interface import MainWindow
+from settings import logger
+# from interface import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+    # window = MainWindow()
+    # window.show()
+    logger.debug(settings.APP_DIR)
+    sys.exit()
---
+++ settings.py
@@ -0,0 +1,38 @@
+import os
+import logging
+ 
+from logging.handlers import RotatingFileHandler
+
+
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DEBUG = False
+
+
+## Logger stuff
+logger = logging.getLogger()
+
+if DEBUG:
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+
+# File log
+file_handler = RotatingFileHandler(os.path.join(APP_DIR, 'log.txt'), 'a', 1000000, 1)
+
+if DEBUG:
+    file_handler.setLevel(logging.DEBUG)
+else:
+    file_handler.setLevel(logging.INFO)
+
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+# Print log on console in DEBUG mode.
+if DEBUG:
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+    logger.addHandler(stream_handler)
+
