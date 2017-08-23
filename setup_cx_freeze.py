8a4ac6d422d07c114b400b0516fbb42f29debb8d
Klafyvel <Klafyvel@users.noreply.github.com>
2017-08-23 07:55:27 +0200

    buil windows

--- setup_cx_freeze.py
+++ setup_cx_freeze.py
@@ -2,9 +2,14 @@ from cx_Freeze import setup, Executable
 import sivicncdriver
 
 # On appelle la fonction setup
+addtional_mods = ['numpy.core._methods', 'numpy.lib.format']
 setup(
     name = "SiviCNCDriver",
     version = sivicncdriver.__version__,
     description = 'A software to control my CNC',
     executables = [Executable("siviCNCDriver.py")],
+    options = {'build_exe': {
+    	'includes': addtional_mods,
+    	'namespace_packages' : ['mpl_toolkits'],
+    }},
 )
